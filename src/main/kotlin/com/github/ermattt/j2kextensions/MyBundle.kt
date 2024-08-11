293df266b4ebdb9b177490faf00c1250c9f35f32
GitHub Action <action@github.com>
2024-08-11 18:16:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/ermattt/j2kextensions/MyBundle.kt
+++ src/main/kotlin/com/github/ermattt/j2kextensions/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ermattt.j2kextensions
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ermattt/j2kextensions/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/ermattt/j2kextensions/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.ermattt.j2kextensions.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
