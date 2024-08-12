9ba2754d7d3dd09d8d99bb3a01d95cb8e3ae6525
Eve Matthaey <ermatt@meta.com>
2024-08-12 10:49:33 -0400

    temp

---
+++ src/main/kotlin/com/github/ermattt/j2kextensions/preprocessors/Temp.java
@@ -0,0 +1,9 @@
+package com.github.ermattt.j2kextensions.preprocessors;
+
+public class Temp {
+    public void doThing(String s) {
+        if (s == null) {
+            System.out.println(s);
+        }
+    }
+}
